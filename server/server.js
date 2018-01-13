06ae93eee3911dfa97c3045e880a389016ac6041
Amy Richardson <amyrichardson96@gmail.com>
2018-01-13 09:11:43 -0600

    server up and running

---
+++ server/server.js
@@ -0,0 +1,18 @@
+//Bring in Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 5400;
+
+//Bring in static files/body parser
+app.use(express.static('./server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Routes
+const tasksRouter = require('./routes/tasks.router');
+app.use('/tasks', tasksRouter)
+
+//Setup Port
+app.listen(PORT, ()=> {
+    console.log('Server up and running on port: ', PORT); 
+});
